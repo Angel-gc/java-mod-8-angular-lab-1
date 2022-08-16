082815f86b81ca5a558f8dfff42946623fdd0ba4
Angel Concepcion <angelconcepcion50@gmail.com>
2022-08-16 15:52:08 -0400

    solution for lab 2

--- my-first-angular-app/src/app/app.component.ts
+++ my-first-angular-app/src/app/app.component.ts
@@ -6,5 +6,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Welcome to the best simple Angular App ever!!';
+  title = 'flatiron-angular';
+  disableNewMessage = true;
+  isActive = false;
+  messages = [
+    {
+      sender: "Ludovic",
+      message: "Latest message from Ludovic"
+    },
+    {
+      sender: "Jessica",
+      message: "Latest message from Jessica"
+    }
+  ]
+  athletes = [
+    {
+      athlete: "Bill Russel",
+      sport: "(Basketball)",
+      status: "is not active"
+    },
+    {
+      athlete: "Gabriela Sabanti",
+      sport: "(Tennis)",
+      status: "is not active"
+    },
+    {
+      athlete: "Simone Biles",
+      sport: "(Olympic Gymnast)",
+      status: "is active"
+    }
+  ]
+  getDisableNewMessage(){
+    return this.disableNewMessage
+  }
+  onSendMessage() {
+    let message = {
+      sender: "Michael",
+      message: "New message from Michael"
+    }
+    this.messages.push(message);
+  }
+  constructor() {
+    console.log("Iniating angular AppCompnent class");
+    setTimeout(() => {
+      this.disableNewMessage = !this.disableNewMessage;
+    }
+    , 2000)
+  }
 }
